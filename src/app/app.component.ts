b36c7c38f5f4fff566b5228e2d614d0f3ac4cca2
Krunal Soni <krunal.soni11@outlook.com>
2024-07-18 17:39:30 +0530

    new components added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    SidebarComponent,
+    MainContentComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
